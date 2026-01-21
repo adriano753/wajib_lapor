535ec44140849a7d6f50b41b9df21694add6b89a
jahongchandra <jahongchandra21@gmail.com>
2026-01-21 15:02:54 +0700

    Finish Jaminan Sosial

---
+++ resources/views/wlkp/upahminimum.blade.php
@@ -0,0 +1,9 @@
+<div class="chart-card full-width mt-4">
+    <div class="chart-header">
+        <h3 class="chart-title">Rata-rata Upah Minimum per Provinsi</h3>
+    </div>
+
+    <div class="chart-container">
+        <canvas id="upahMinimumChart"></canvas>
+    </div>
+</div>
